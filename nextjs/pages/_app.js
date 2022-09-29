a7dcc548bafb01ed96bbeff6db06da65ee39210a
laney522 <pm72222@gmail.com>
2022-09-30 08:24:35 +0900

    update:

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import 'semantic-ui-css/semantic.min.css'
 import Footer from '../src/component/Footer'
+import Top from '../src/component/Top'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Top />
       <Component {...pageProps} />
       <Footer />
     </div>
