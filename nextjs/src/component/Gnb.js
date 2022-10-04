07bfb238344b19578f218b7c40ee175d3c601733
laney522 <pm72222@gmail.com>
2022-10-04 23:33:11 +0900

    update Top.js

--- nextjs/src/component/Gnb.js
+++ nextjs/src/component/Gnb.js
@@ -1,22 +1,23 @@
 import { Menu } from "semantic-ui-react";
 
 export default function Gnb() {
+  const activeItem = "home";
   return (
     <Menu inverted>
       <Menu.Item
         name="home"
         active={activeItem === 'home'}
-        onClick={this.handleItemClick}
+      // onClick={this.handleItemClick}
       />
       <Menu.Item
         name='messages'
         active={activeItem === 'messages'}
-        onClick={this.handleItemClick}
+      // onClick={this.handleItemClick}
       />
       <Menu.Item
         name='friends'
         active={activeItem === 'friends'}
-        onClick={this.handleItemClick}
+      // onClick={this.handleItemClick}
       />
     </Menu>
   )
--- nextjs/src/component/Top.js
+++ nextjs/src/component/Top.js
@@ -1,8 +1,14 @@
+import { Header } from 'semantic-ui-react';
+import Gnb from './Gnb';
+
 export default function Top() {
   return (
-    <div>
+    <div style={{ display: "flex" }}>
+
+
       <img src="/images/laney.jpg" alt="logo" />
       <Headers as="h1">Laney</Headers>
+      <Gnb />
     </div>
   );
 }
