4ab31029646e6227ab04f1a495e1b45e4df99288
laney522 <pm72222@gmail.com>
2022-10-14 23:41:18 +0900

    Top.js Gnb component adjust location

--- nextjs-tutorial/pages/_app.js
+++ nextjs-tutorial/pages/_app.js
@@ -5,16 +5,15 @@ import Top from '../src/component/Top'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <div style={{ width: 1000, margin: "0 auto" }}>
       <Top />
       <Component {...pageProps} />
       <Footer />
     </div>
-  )
-
+  );
 }
 
-export default MyApp
+export default MyApp;
 
 /**
  * 페이지 전환시 레이아웃을 유지할 수 있습니다.
--- nextjs-tutorial/src/component/Top.js
+++ nextjs-tutorial/src/component/Top.js
@@ -4,8 +4,16 @@ import Gnb from './Gnb';
 export default function Top() {
   return (
     <div>
-      <img src="/images/laney.png" alt="logo" />
-      <Header as="h1">Laney</Header>
+      <div style={{ display:'flex', paddingTop: 20 }}>
+        <div style={{ flex: "100px 0 0" }}>
+          <img
+            src="/images/laney.png"
+            alt="logo"
+            style={{ display: "block", width: 80 }}
+          />
+        </div>
+          <Header as="h1">Laney</Header>
+      </div>
       <Gnb />
     </div>
   );
