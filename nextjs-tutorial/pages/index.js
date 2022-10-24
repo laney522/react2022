2745559c548851be77b14cd17be3eb4bb36a0d05
laney522 <pm72222@gmail.com>
2022-10-24 23:21:35 +0900

    updated index.js

--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
     axios.get(API_URL).then((res) => {
       console.log(res.data);
       setList(res.data);
+      setIsLoading(false);
     });
   }
 
@@ -26,17 +27,21 @@ export default function Home() {
   return (
     <div>
       {/* <title>HOME | Laney</title> */}
-      <Divider />
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        베스트 상품
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(0, 9)} />
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        신상품
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(9)} />
+      {isLoading && (
+        <>
+          <Divider />
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            베스트 상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(0, 9)} />
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            신상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(9)} />
+        </>
+      )}
     </div>
   );
 }
