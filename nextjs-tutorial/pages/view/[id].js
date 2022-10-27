7b98cea836431af4789db79662cc33b202689022
laney522 <pm72222@gmail.com>
2022-10-27 21:54:49 +0900

    next/link updated

--- nextjs-tutorial/pages/view/[id].js
+++ nextjs-tutorial/pages/view/[id].js
@@ -1,32 +1,24 @@
 import axios from 'axios';
 import Axios from 'axios';
+import Head from 'next/head';
 import { useRouter } from "next/router";
 import { useEffect, useState } from 'react';
 import Item from '../../src/component/Item';
 
-const Post = () => {
-  const router = useRouter();
-  const { id } = router.query;
-
-  const [item, setItem] = useState({});
-
-  // const API_URL = `http://makeup-api.herekuapp.com/api/v1/products/${id}.json`; ... typing error
-  const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
-
-  function getData() {
-    Axios.get(API_URL).then((res) => {
-      setItem(res.data);
-      console.log(res.data);
-    });
-  };
-
-  useEffect(() => {
-    if (id && id > 0) {
-      getData();
-    }
-  }, [id]);
-
-  return <Item item={item} />;
+const Post = ({ item }) => {
+  return (
+    <>
+      {item && (
+        <>
+          <Head>
+            <title>{item.name}</title>
+            <meta name="description" content={item.description}></meta>
+          </Head>
+        </>
+      )}
+      <Item item={item} />;
+    </>
+  );
 };
 
 export default Post;
