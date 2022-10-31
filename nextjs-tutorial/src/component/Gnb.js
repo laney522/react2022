029c0645c68f235b39c2485d840216293dbe134a
laney522 <pm72222@gmail.com>
2022-10-31 23:18:12 +0900

    add error page 404, 500

--- nextjs-tutorial/src/component/Gnb.js
+++ nextjs-tutorial/src/component/Gnb.js
@@ -5,7 +5,7 @@ export default function Gnb() {
   const router = useRouter();
   console.log(router);
   let activeItem;
-  
+
   if (router.pathname === '/') {
     activeItem = "home";
   } else if (router.pathname === '/about') {
@@ -16,22 +16,24 @@ export default function Gnb() {
     if (data.name === 'home') {
       router.push("/");
     } else if (data.name === 'about') {
-      // router.push("/about");
-      location.href = "/about";
+      router.push("/about");
     }
   }
 
   return (
     <Menu inverted>
-      <Menu.Item
-        name='home'
-        active={activeItem === 'home'}
-        onClick={goLink}
-      />
+      <Menu.Item name="home" active={activeItem === "home"} onClick={goLink} />
       <Menu.Item
         name='about'
         active={activeItem === 'about'}
         onClick={goLink} />
+      <Menu.Item
+        name='Contact Us'
+        active={activeItem === 'contact'}
+        onClick={() => {
+          router.push("/contact");
+        }}
+      />
     </Menu>
   );
 }
