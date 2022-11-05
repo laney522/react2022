f0cad23372c5cf2dafe45fcef4a5291f2c8aa4fb
laney522 <pm72222@gmail.com>
2022-11-05 23:12:29 +0900

    find error 1

--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -4,38 +4,16 @@ import styles from '../styles/Home.module.css';
 import axios from 'axios';
 import ItemList from '../src/component/ItemList';
 import { Divider, Header, Loader } from 'semantic-ui-react';
+import Head from "next/head"
 
-export default function Home() { 
-  const [list, setList] = useState([]); 
-  const [isLoading, setIsLoading] = useState(true); 
-
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-  function getData() { 
-    axios.get(API_URL).then((res) => { 
-      console.log(res.data); 
-      setList(res.data); 
-      setIsLoading(false);
-    }); 
-  } 
-
-  useEffect(() => {
-    getData();
-  }, []);
-
+export default function Home({ list }) {
   return (
     <div>
-      {/* <title>HOME | Laney</title> */}
-      {isLoading && (
-        <div style={{ padding: "300px 0" }}>
-          <Loader inline="centered" active>
-            Loading
-          </Loader>
-        </div>
-      )}
-      {!isLoading && (
+      <Head>
+        <title>Home | laney</title>
+        <meta name="description" content="laney의 홈입니다."></meta>
+      </Head>
         <>
-          <Divider />
           <Header as="h3" style={{ paddingTop: 40 }}>
             베스트 상품
           </Header>
@@ -47,7 +25,19 @@ export default function Home() {
           <Divider />
           <ItemList list={list.slice(9)} />
         </>
-      )}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const apiUrl = process.env.apiUrl;
+  const res = await axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      list: data,
+      name: process.env.name
+    },
+  };
 }
