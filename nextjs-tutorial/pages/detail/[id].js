e252dfe561c7806905cced1e90cda9618359105b
laney522 <pm72222@gmail.com>
2022-11-08 23:22:32 +0900

    update [id].js

--- nextjs-tutorial/pages/detail/[id].js
+++ nextjs-tutorial/pages/detail/[id].js
@@ -1,10 +1,23 @@
-import axios from 'axios';
+import Axios from 'axios';
 import Head from 'next/head';
 import { useRouter } from "next/router";
 import { useEffect, useState } from 'react';
+import { Loader } from 'semantic-ui-react';
 import Item from '../../src/component/Item';
 
 const Post = ({ item, name }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return (
+      <div style={{ padding: "100px 0" }}>
+        <Loader active inline="centered">
+          Loading
+        </Loader>
+      </div>
+    );
+  }
+
   return (
     <>
       {item && (
@@ -23,13 +36,21 @@ const Post = ({ item, name }) => {
 
 export default Post;
 
-export async function getStaticPaths(){
+export async function getStaticPaths() {
+  const apiUrl = process.env.apiUrl;
+  const res = await Axios.get(apiUrl);
+  const data = res.data;
   return {
-    paths: [
-      { params: { id: '740' } },
-      { params: { id: '730' } },
-      { params: { id: '729' } },
-    ],
+    // paths: [
+    //   { params: { id: '740' } },
+    //   { params: { id: '730' } },
+    //   { params: { id: '729' } },
+    // ],
+    paths: data.slice(0, 9).map(item => ({
+      params: {
+        id: item.id.toString(),
+      },
+    })),
     fallback: true,
   };
 }
@@ -37,7 +58,7 @@ export async function getStaticPaths(){
 export async function getStaticProps(context) {
   const id = context.params.id;
   const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
-  const res = await axios.get(apiUrl);
+  const res = await Axios.get(apiUrl);
   const data = res.data;
 
   return {
