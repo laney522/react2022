cdb282077e7176964868a1f26ac084b714d2060f
laney522 <pm72222@gmail.com>
2022-11-13 23:08:27 +0900

    admin page update

--- nextjs-tutorial/pages/admin.js
+++ nextjs-tutorial/pages/admin.js
@@ -1,3 +1,20 @@
+import Axios from "axios";
+import { useEffect } from "react";
+
 export default function Admin() {
+  function checkLogin(){
+    Axios.get('/api/isLogin')
+    .then(res => {
+      if(res.status === 200 && res.data.name) {
+        //로그인
+      } else {
+        //로그인 안됨
+      }
+    })
+  }  
+  
+  useEffect(()=> {
+     checkLogin(); 
+    },[])
   return <>admin</>;
 }
