cc9966dd5b0ace6b488ddbe709a2d2b1528c8b18
laney522 <pm72222@gmail.com>
2022-11-17 22:16:13 +0900

    test1

--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -7,22 +7,22 @@ import Head from "next/head"
 export default function Home({ list }) {
 
   return (
-    <div>
-      <Head>
-        <title>Home | laney</title>
-        <meta name="description" content="laney의 홈입니다."></meta>
-      </Head>
-        <>
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            베스트 상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(0, 9)} />
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            신상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(9)} />
+    <div> 
+      <Head> 
+        <title>Home | laney</title> 
+        <meta name="description" content="laney의 홈입니다."></meta> 
+      </Head> 
+        <> 
+          <Header as="h3" style={{ paddingTop: 40 }}> 
+            베스트 상품 
+          </Header> 
+          <Divider /> 
+          <ItemList list={list.slice(0, 9)} /> 
+          <Header as="h3" style={{ paddingTop: 40 }}> 
+            신상품 
+          </Header> 
+          <Divider /> 
+          <ItemList list={list.slice(9)} /> 
         </>
     </div>
   );
